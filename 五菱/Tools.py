8fa57e555d16ceb1bda53d70797c52f33ca14102
阿银 <3249802022ljl@gmail.com>
2025-08-29 18:51:51 +0800

    Delete 五菱 directory

--- 五菱/Tools.py
+++
@@ -1,6 +0,0 @@
-import string
-import random
-def random_string(length: int) -> str:
-    chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(chars) for _ in range(length))
-print(random_string(7))
